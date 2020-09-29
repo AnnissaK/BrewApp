c66be812f9c42878799cbff4e3d83eac548ee8d2
Annissa3 <annissak@hotmail.co.uk>
2020-09-29 14:01:41 +0100

    wouldnt let me move branch to my masters

--- classes_people.py
+++ classes_people.py
@@ -1,8 +1,12 @@
-class Person():
-     def __init__(self,name):
-         self.name = name
-drinks =[]
-people =[]
+import csv
+from csv import DictReader
+from csv import writer
+from csv import reader
+# class Person():
+#      def __init__(self,name):
+#          self.name = name
+# drinks =[]
+# people =[]
 # people=[]
 # name =['bil','bob','susan']
 
@@ -34,56 +38,68 @@ people =[]
 #     name_of_drink =get_name_of_person(round_handle_user_input(print_drinks(),'drink'),print_drinks())
 #     return owner_name,order_person,name_of_drink
 
-def load_people():
-    for i,s in zip(load_into_list(file_people_list,'name'),load_into_list(file_people_list,'age')):
-        people.append(Person(i,s))
+
+def load_into_list(filename,header):
+    data_list =[]
+    with open(filename,'r') as csvfile:
+        csv_dict_reader = DictReader(csvfile)
+        for row in csv_dict_reader:
+            data_list.append(row[header])
+        return data_list
+
+print(load_into_list('person.csv','age'))
+
+
+# def load_people():
+#     for i,s in zip(load_into_list(file_people_list,'name'),load_into_list(file_people_list,'age')):
+#         people.append(Person(i,s))
     
         
-def print_people():
-    people_names = [person.name for person in people]
-    return people_names
+# def print_people():
+#     people_names = [person.name for person in people]
+#     return people_names
 
-def get_age_of_person():
-    person_age = [person.age for person in people]
-    return person_age
+# def get_age_of_person():
+#     person_age = [person.age for person in people]
+#     return person_age
 
   
-def print_drinks():
-    drinks_name = [drink_name.drink for drink_name in drinks]
-    return drinks_name
+# def print_drinks():
+#     drinks_name = [drink_name.drink for drink_name in drinks]
+#     return drinks_name
 
-def get_type_of_drink():
-    type_of_drink = [typedrink.type for typedrink in drinks]
-    return type_of_drink
-
-
-
-
-def load_into_round_class(x,y,z):
-    order_requests=[]
-    for owner,name,drink in zip(x,y,z):
-        R1=Round(owner)
-        R1.add_order(name,drink)
-        order_requests.append(R1)
-    return order_requests
+# def get_type_of_drink():
+#     type_of_drink = [typedrink.type for typedrink in drinks]
+#     return type_of_drink
 
 
 
-def round_handle_user_input(list_data,element):
-    for i,x in enumerate (list_data):
-        print(i,x)
-    input_number = input(f'Select the number corresponding to the {element} you want from the list \n')
-    return input_number
+
+# def load_into_round_class(x,y,z):
+#     order_requests=[]
+#     for owner,name,drink in zip(x,y,z):
+#         R1=Round(owner)
+#         R1.add_order(name,drink)
+#         order_requests.append(R1)
+#     return order_requests
+
+
+
+# def round_handle_user_input(list_data,element):
+#     for i,x in enumerate (list_data):
+#         print(i,x)
+#     input_number = input(f'Select the number corresponding to the {element} you want from the list \n')
+#     return input_number
     
-def get_name_of_person(name,list_data):
-    index = int(name)
-    element_value =list_data[index]
-    return element_value
+# def get_name_of_person(name,list_data):
+#     index = int(name)
+#     element_value =list_data[index]
+#     return element_value
 
-def handle_round_input():
-    owner_name =get_name_of_person(round_handle_user_input(print_people(),'round owner'),print_people())
-    order_person = get_name_of_person(round_handle_user_input(print_people(),'owners orderees name'),print_people())
-    name_of_drink =get_name_of_person(round_handle_user_input(print_drinks(),'drink'),print_drinks())
-    return owner_name,order_person,name_of_drink
+# def handle_round_input():
+#     owner_name =get_name_of_person(round_handle_user_input(print_people(),'round owner'),print_people())
+#     order_person = get_name_of_person(round_handle_user_input(print_people(),'owners orderees name'),print_people())
+#     name_of_drink =get_name_of_person(round_handle_user_input(print_drinks(),'drink'),print_drinks())
+#     return owner_name,order_person,name_of_drink
     
-print(handle_round_input())
+# print(handle_round_input())
